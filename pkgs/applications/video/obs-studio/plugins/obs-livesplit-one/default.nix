3a424522a8357a3c232df694419357e9e166347d
Colin Heffernan <colinpheffernan@gmail.com>
2024-01-20 03:28:45 -0500

    `obs-livesplit-one` 0.2.0 -> 0.3.4

--- pkgs/applications/video/obs-studio/plugins/obs-livesplit-one/default.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-livesplit-one/default.nix
@@ -1,27 +1,27 @@
-{ lib
-, fetchFromGitHub
-, rustPlatform
-, cmake
-, fontconfig
-, obs-studio
-, pkg-config
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  cmake,
+  fontconfig,
+  obs-studio,
+  pkg-config,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "obs-livesplit-one";
-  version = "0.2.0";
+  version = "0.3.4";
 
   src = fetchFromGitHub {
-    owner = "CryZe";
+    owner = "LiveSplit";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-C1u4z7iQUETM84kf6S6obw+C0ox8J9gMJoVP3/3ZoYw=";
+    sha256 = "";
   };
 
   cargoHash = "sha256-mQ0TR4DL4bA5u4IL3RY9aLxU5G6qQ5W5xuNadiXGeB0=";
 
-  nativeBuildInputs = [ cmake pkg-config ];
-  buildInputs = [ fontconfig obs-studio ];
+  nativeBuildInputs = [cmake pkg-config];
+  buildInputs = [fontconfig obs-studio];
 
   postInstall = ''
     mkdir $out/lib/obs-plugins/
@@ -30,9 +30,9 @@ rustPlatform.buildRustPackage rec {
 
   meta = with lib; {
     description = "OBS Studio plugin for adding LiveSplit One as a source";
-    homepage = "https://github.com/CryZe/obs-livesplit-one";
-    license = with licenses; [ asl20 mit ];
-    maintainers = [ maintainers.Bauke ];
+    homepage = "https://github.com/LiveSplit/obs-livesplit-one";
+    license = with licenses; [asl20 mit];
+    maintainers = [maintainers.Bauke];
     platforms = obs-studio.meta.platforms;
   };
 }
